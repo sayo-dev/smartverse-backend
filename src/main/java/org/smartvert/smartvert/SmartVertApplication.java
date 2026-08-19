bf6af94a79cae0125a2cd8d4a8601e9c299c5482
sayo-dev <akinsam2443@gmail.com>
2026-08-19 17:28:43 +0100

    ~add initial project structure and configuration files

---
+++ src/main/java/org/smartvert/smartvert/SmartVertApplication.java
@@ -0,0 +1,13 @@
+package org.smartvert.smartvert;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartVertApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartVertApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/smartvert/smartvert/SmartVertApplicationTests.java
@@ -0,0 +1,13 @@
+package org.smartvert.smartvert;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartVertApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
